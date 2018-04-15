ac6043c993ec2dd92df26143a462334e1b6cc909
Magdalena Gresk <magdagresk@onet.pl>
2018-04-14 22:16:06 -0400

    Modify scripts.js: correct tags

--- scripts.js
+++ scripts.js
@@ -6,10 +6,10 @@ var cauntriesLanguage = $('#languages[0].name')
 var cauntriesCurrencies = $('#currencies[0].name')
 
 function searchCountries() {
-  var countryName = $('#country-name').val()
-  if(!countryName.length) countryName = 'Poland'
-  $.ajax({
-  		url: url + countryName,
+    var countryName = $('#country-name').val()
+    if(!countryName.length) countryName = 'Poland'
+    $.ajax({
+        url: url + countryName,
   		method: 'GET',
   		success: showCountriesList
   	})
@@ -22,7 +22,7 @@ function showCountriesList(resp) {
 function showCountriesList(resp) {
     countriesList.empty()
     resp.forEach(function(item){
-   		$('<h3>').text(item.name).appendTo(countriesList)
+        $('<h3>').text(item.name).appendTo(countriesList)
    		$('<h5>').text('capital: ' + item.capital).appendTo(countriesList)
    		$('<h5>').text('region: ' + item.region).appendTo(countriesList)
    		$('<h5>').text('official language: ' + item.languages[0].name).appendTo(countriesList)
